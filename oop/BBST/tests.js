83ea2a668cf73f3fcb46711ff446266a0b546d21
Catalin Costan <cov@ad.rms.intern>
2018-11-07 07:52:54 +0200

    WIP: tests are now accurate, changes to be made

--- oop/BBST/tests.js
+++ oop/BBST/tests.js
@@ -50,13 +50,6 @@ QUnit.test("height", function( assert ) {
     assert.ok( res.leftChild.height < res.height < res.parent.height, "Passed!" );
 });
 
-QUnit.test("deleting leaf", function( assert ) {
-    let binaryTree = getInit();
-    let res = binaryTree.delete(5);
-    assert.ok( res && binaryTree.dfs().indexOf(5) == -1, "Passed!" );
-    binaryTree.add(5);
-});
-
 // --- tests for rotations on add
 QUnit.test("rotation -- RR", function(assert) {
     let binaryTree = getInit();
@@ -64,23 +57,23 @@ QUnit.test("rotation -- RR", function(assert) {
     binaryTree.add(102);
     binaryTree.add(103);
     let res = binaryTree.dfs();
-    const expected = [4, 2, 0, -1, 1, 3, 5, 101, 102, 103];
+    // const actual = [4, 2, 0, -1, 1, 3, 5, 101, 102, 103];
+    const expected = [2, 0, -1, 1, 101, 4, 3, 5, 102, 103];
 
     assert.ok( arrayEquality(res, expected), "Passed!");
 });
 
-// --- tests for rotations on add
 QUnit.test("rotation -- LL", function(assert) {
     let binaryTree = getInit();
     binaryTree.add(-2);
     binaryTree.add(-3);
     binaryTree.add(-4);
     let res = binaryTree.dfs();
-    const expected1 = [-1, -2, -3, -4, 2, 0, 1, 4, 3, 5]; 
-    assert.ok( arrayEquality(res, expected1) || arrayEquality(res, expected2), "Passed!");
+    // const actual = [-1, -2, -3, -4, 2, 0, 1, 4, 3, 5]; 
+    const expected = [2, -3, -2, -1, 0, -4, 1, 4, 3, 5];
+    assert.ok( arrayEquality(res, expected), "Passed!");
 });
 
-//--- tests for rotations on add
 QUnit.test("rotation -- LR", function(assert) {
     let binaryTree = getInit();
     binaryTree.add(-5);
@@ -88,24 +81,37 @@ QUnit.test("rotation -- LR", function(assert) {
     binaryTree.add(-3);
     
     let res = binaryTree.dfs();
-    const expected = [-1, -5, -2, -3, 2, 0, 1, 4, 3, 5];
+    // const actual = [-1, -5, -2, -3, 2, 0, 1, 4, 3, 5];
+    const expected = [2, -5, -2, -1, -3, 0, 1, 4, 3, 5];
     assert.ok( arrayEquality(res, expected), "Passed!");
 });
 
-//--- tests for rotations on add
 QUnit.test("rotation -- RL", function(assert) {
     let binaryTree = getInit();
     binaryTree.add(8);
     binaryTree.add(7);
     
     let res = binaryTree.dfs();
-    const expected = [2, 0, -1, 1, 5, 4, 3, 8, 7];
+    // const actual = [2, 0, -1, 1, 5, 4, 3, 8, 7];
+    const expected = [2, 0, -1, 1, 4, 3, 7, 5, 8];
     assert.ok( arrayEquality(res, expected), "Passed!");
+});
+
+// --- tests for deletion
+QUnit.test("deleting leaf", function( assert ) {
+    let binaryTree = getInit();
+    let res = binaryTree.delete(5);
+    let dfs = binaryTree.dfs();
+    const expected = [2, 0, -1, 1, 4, 3];
+    
+    assert.ok( res && dfs.indexOf(5) == -1 && arrayEquality(dfs, expected), "Passed!" );
 });
 
 QUnit.test("deleting node", function( assert ) {
     let binaryTree = getInit();
-    let res = binaryTree.delete(5);
-    assert.ok( res && binaryTree.dfs().indexOf(5) == -1, "Passed!" );
-    binaryTree.add(5);
+    let res = binaryTree.delete(2);
+    let dfs = binaryTree.dfs();
+    const expected = [1, 0, -1, 4, 3, 5];
+
+    assert.ok( res && dfs.indexOf(2) == -1 && arrayEquality(dfs, expected), "Passed!" );
 });
